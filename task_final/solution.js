c39a073f541b8b50a0297eccc841f58c287c3ef3
AntonVDB1 <farkind@yandex.ru>
2022-02-14 00:00:36 +0300

    final task is done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,33 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {
-        "client": "Иван, +7(987)65-43-210",
-        'goods': [
-            {
-            "title": "Пицца",
-            "count": 2
-        }
-
-        ], 
-        'order': {
-            "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-            "sum": 900
-        }
-    };
+    let data = {client: "", order: {}, goods: [] };
 
     let countOfGoods = goods.length;
-    
-    for (let i = 0; i <= countOfGoods; i => 1) {
-        data.goods.push(goods[i].title);
+
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.client = name + ' ' + phone;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
